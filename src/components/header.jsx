0232ae269cf357b987d9c9ecc944e4f25a04b0be
Juan Giovanni Restrepo <grestrepojimenez@gmail.com>
2020-07-01 22:57:19 -0500

    Ajustes bootstrap en visualizacion de componentes

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,25 +2,24 @@ import React from 'react'
 
 const Header = () => (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed">
-        <div className="container">
             <a className="navbar-brand" href="/"> Shopping React</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarResponsive">
-                <ul className="navbar-nav ml-authref">
+            
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav ml-auto">
                     <li className="nav-item active">
-                        <a className="nav-a" href="/">Home</a>
+                        <a className="nav-link" href="/">Home </a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-a" href="/register">Register</a>
+                        <a className="nav-link" href="/register">Register</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-a" href="/login">Login</a>
+                        <a className="nav-link" href="/login">Login</a>
                     </li>
                 </ul>
             </div>
-        </div>
     </nav>
 )
 
